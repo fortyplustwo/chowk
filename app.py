bb7f35dd1d3b4ae8da5e77b68cd417d9408b526f
Samarendra M Hedaoo <knightsamar@gmail.com>
2015-02-23 16:21:46 +0530

    Replacing all instances of report_X_to_rapidpro with report_status_to_rapidpro.

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def sendsms():
         else:
             #report back to the RapidPro server about the success of delivery of this message
             app.logger.debug("Message %s succesfully forwarded to Kannel server", msg)
-            report_sent_to_rapidpro(msg = msg, app = app)
+            report_status_to_rapidpro(status = 'SENT', msg = msg, app = app)
             #we return in the format (response, status, headers) so that RapidPro knows that everything is HTTP 200 :)
             return ('',200,[])
     except KeyError as e:
