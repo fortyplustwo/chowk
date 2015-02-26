8301b856908020112f749f95e9ac20dd84f947c5
Samarendra M Hedaoo <samarendra.hedaoo@gmail.com>
2015-02-26 15:10:08 +0530

    Removing unnecessary boiler plate code from celery_maker

--- celery_maker.py
+++ celery_maker.py
@@ -6,14 +6,16 @@ def make_celery():
     celery = Celery('app', broker = CELERY_BROKER_URL)
     TaskBase = celery.Task
     
+    '''
     class ContextTask(TaskBase):
         abstract = True
         def __call__(self, *args, **kwargs):
             with app.app_context():
                 return TaskBase.__call__(self, *args, **kwargs)
-            
+
     
     celery.Task = ContextTask
+    '''
     return celery
 
 celery = make_celery()
